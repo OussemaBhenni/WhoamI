a85250f037c0a799ba7338d34903a1b82d09fe11
OussemaBhenni <oussemabenhenni@gmail.com>
2022-05-02 11:23:08 +0000

    All the project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ================================= typing ===============================*/
+var typed = new Typed(".typing",{
+  strings:["Front Web Developper","Web Designer","Freelancer"],
+  typeSpeed:100,
+  backSpeed:60,
+  loop:true
+})
